3ee7dfeaae3d2a2daa133cfdb2621a814c64ec08
srjOfficial <suraj2office@duck.com>
2023-12-09 23:35:00 +0530

    Rename and fix

--- in28min/src/main/java/com/suraj/in28min/code/main/App01GamingBasicJava.java
+++ in28min/src/main/java/com/suraj/in28min/code/main/App01GamingBasicJava.java
@@ -5,7 +5,7 @@ import com.suraj.in28min.code.game.MarioGame;
 import com.suraj.in28min.code.game.Pacman;
 import com.suraj.in28min.code.game.SuperContra;
 
-public class AppGamingBasicJava {
+public class App01GamingBasicJava {
 
 	public static void main(String[] args) {
 		// var is keyword to get Datatype based on context introduced in Java 10
@@ -14,14 +14,13 @@ public class AppGamingBasicJava {
 		 * GameConsole interface now do not need any change in GameRunner Class to run
 		 * both games
 		 */
-		var game = new MarioGame(); // this represents: GameConsole game = new MarioGame();
+		/*
+		 * var game1 = new MarioGame(); 
+		 * var gameRunner1 = new GameRunner(game1); // This will have gameRunner refer to SuperContra gameRunner1.run();
+		 */
+		var game = new Pacman(); // this represents: GameConsole game = new MarioGame();
 
 		var gameRunner = new GameRunner(game); // This will have gameRunner refer to marioGame
-		var game1 = new Pacman();
-
-		var gameRunner1 = new GameRunner(game1); // This will have gameRunner refer to SuperContra
-		gameRunner.run();
-		gameRunner1.run();
 
 	}
 
