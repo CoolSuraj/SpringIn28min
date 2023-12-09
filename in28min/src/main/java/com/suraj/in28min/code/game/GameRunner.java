df621d89a3ff8bd88ebd8142aaa6946cc9d5a754
srjOfficial <suraj2office@duck.com>
2023-12-09 21:53:33 +0530

    Example to show Tight Coupling

--- in28min/src/main/java/com/suraj/in28min/code/game/GameRunner.java
+++ in28min/src/main/java/com/suraj/in28min/code/game/GameRunner.java
@@ -1,12 +1,16 @@
 package com.suraj.in28min.code.game;
 
 public class GameRunner {
+	/**
+	 * This is example to show tight coupling like
+	 * You can take a laptop anywhere but not desktop that easily
+	 * 
+	 */
+	public SuperContra game; //Everytime we have to change the class here for all games this is called Tight coupling
+	
+	public GameRunner(SuperContra superContra) {
 
-	public MarioGame game;
-
-	public GameRunner(MarioGame game) {
-
-		this.game = game; 
+		this.game = superContra; 
 	}
 
 	public GameRunner() {
--- in28min/src/main/java/com/suraj/in28min/code/main/AppGamingBasicJava.java
+++ in28min/src/main/java/com/suraj/in28min/code/main/AppGamingBasicJava.java
@@ -2,13 +2,15 @@ package com.suraj.in28min.code.main;
 
 import com.suraj.in28min.code.game.GameRunner;
 import com.suraj.in28min.code.game.MarioGame;
+import com.suraj.in28min.code.game.SuperContra;
 
 public class AppGamingBasicJava {
 
 	public static void main(String[] args) {
 		// var is keyword to get Datatype based on context introduced in Java 10
-		var marioGame = new MarioGame();
-		var gameRunner = new GameRunner(marioGame); //This will have gameRunner refer to marioGame 
+		//var marioGame = new MarioGame();
+		var superContra  = new SuperContra();
+		var gameRunner = new GameRunner(superContra); //This will have gameRunner refer to marioGame 
 		gameRunner.run(); //run called for marioGame ?
 		
 		
