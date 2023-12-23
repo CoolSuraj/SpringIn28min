59ef47f56c7f862857540d93b5a59a1400428d37
srjOfficial <suraj2office@duck.com>
2023-12-23 14:23:28 +0530

    Setting up first Spring boot project with spring initializer

---
+++ learn-spring-boot-1/src/test/java/com/suraj/in28min/springbooot/learnspringboot1/LearnSpringBoot1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suraj.in28min.springbooot.learnspringboot1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringBoot1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
