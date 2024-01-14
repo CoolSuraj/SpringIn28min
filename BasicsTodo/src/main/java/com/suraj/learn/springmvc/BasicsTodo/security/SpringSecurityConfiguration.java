72916c3d56e360ebaa373a6ed70d90f7488c93d6
srjOfficial <suraj2office@duck.com>
2024-01-14 21:47:23 +0530

    using passWordEncoder

--- BasicsTodo/src/main/java/com/suraj/learn/springmvc/BasicsTodo/security/SpringSecurityConfiguration.java
+++ BasicsTodo/src/main/java/com/suraj/learn/springmvc/BasicsTodo/security/SpringSecurityConfiguration.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
@@ -17,13 +19,31 @@ public class SpringSecurityConfiguration {
 	@Bean
 	InMemoryUserDetailsManager CreateUserDetailsManager() {
 		/**
-		 * this is depricated method but just used for demo
+		 * this (withDefaultPasswordEncoder()) is depricated method but just used for demo but when we use passwordEncoder() then we might get issue
 		 */
 		UserDetails userDetails = User.withDefaultPasswordEncoder()
 				.username("surya").password("dummy").roles("USER", "ADMIN")
 				.build();
+		/**
+		 * hence to not get issue due to passwordEncoder() we use below implementation using below one should not give problem
+		 */
+		UserDetails userDetails2 = User.builder().passwordEncoder(input->passwordEncoder().encode(input))
+				.username("user").password("pass1").roles("USER", "ADMIN")
+				.build();
 		
-		return new InMemoryUserDetailsManager(userDetails);
+		UserDetails[] users = {userDetails,userDetails2};
+		return new InMemoryUserDetailsManager(users);
+	}
+	
+	/**
+	 * Let's use something which is not depricated
+	 */
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		
+		
+		return new BCryptPasswordEncoder();
 	}
 	
 }
