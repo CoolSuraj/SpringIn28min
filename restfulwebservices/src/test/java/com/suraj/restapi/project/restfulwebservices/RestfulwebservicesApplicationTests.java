00697f41582dfc4f178a1ac6bf09baed70dffa60
srjOfficial <suraj2office@duck.com>
2024-03-17 20:54:41 +0530

    created new App for study of rest api

---
+++ restfulwebservices/src/test/java/com/suraj/restapi/project/restfulwebservices/RestfulwebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suraj.restapi.project.restfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulwebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
