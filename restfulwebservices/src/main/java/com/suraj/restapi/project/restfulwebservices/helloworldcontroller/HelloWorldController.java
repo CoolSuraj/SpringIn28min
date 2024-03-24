782cd55eb8bea119af25e67a134251e822e15a21
srjOfficial <suraj2office@duck.com>
2024-03-24 23:13:16 +0530

    internalization

--- restfulwebservices/src/main/java/com/suraj/restapi/project/restfulwebservices/helloworldcontroller/HelloWorldController.java
+++ restfulwebservices/src/main/java/com/suraj/restapi/project/restfulwebservices/helloworldcontroller/HelloWorldController.java
@@ -1,5 +1,10 @@
 package com.suraj.restapi.project.restfulwebservices.helloworldcontroller;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +16,9 @@ import com.suraj.restapi.project.restfulwebservices.dto.HelloWorld;
 // This controller is Rest APi Exposer 
 @RestController
 public class HelloWorldController {
+	
+	@Autowired
+	private MessageSource messageSource;
 	
 	// for /hello-world we should return "Hello World" first Hello World API
 	@RequestMapping(method=RequestMethod.GET , path ="/hello-world")
@@ -28,6 +36,12 @@ public class HelloWorldController {
 	@GetMapping("/hello-world-bean/path-variable/{name}")
 	public HelloWorld helloWorldBeanPathVariable(@PathVariable String name) {
 		return new HelloWorld("Hello World "+name);
+	}
+	
+	@RequestMapping(method=RequestMethod.GET , path ="/good-morning")
+	public String GoodMorningInternational() {
+		Locale locale = LocaleContextHolder.getLocale();
+		return messageSource.getMessage("good.morning.message", null, "default Good Morning", locale);
 	}
 	
 }
